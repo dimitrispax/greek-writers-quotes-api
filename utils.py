090a034e56efecaef2df5e949f392f5622405c62
Dimitris Pax <dpaximadakis@bestprice.gr>
2024-12-11 22:57:10 +0200

    refactor: changed comments

--- utils.py
+++ utils.py
@@ -1,24 +1,21 @@
 import json
 import random
 
-# Path to the JSON file
 json_file_path = './data/quotes.json'
 
-# Load JSON data from the file
+# Load the quotes from the file containing them
 with open(json_file_path, 'r') as file:
   quotes = json.load(file)
 
 def get_randomized_quote():
-  # Generate a random integer that will become the index of the shuffled list.
   random_index = random.randint(0, len(quotes) - 1)
-
-  # Shuffle the quote list 
+  
+  # Suffling the quotes list to increase the entropy
   random.shuffle(quotes)
 
   return quotes[random_index]
 
 def get_shuffled_quotes():
-  # Shuffle the quote list 
   random.shuffle(quotes)
 
   return quotes
